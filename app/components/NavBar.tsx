dfb378a3a02c9760ebf2ffdcd15d572fc7583e5b
calebtc <calebthecurious@gmail.com>
2023-06-27 16:27:54 +0700

    22nd adding material UI to Login & signUp function

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,14 +1,15 @@
 "use client";
 
 import Link from "next/link";
+import LoginModal from "./LoginModal";
 
 export default function NavBar() {
     return (
         <nav className="bg-white p-2 flex justify-between">
             <Link href="/" className='font-bold text-gray-700 text-2xl'>Locus</Link>
             <div className="flex">
-                <button className='bg-blue-400 text-white border p-1 px-4 rounded mr-3'>Sign In</button>
-                <button className='border p-1 px-4 rounded'>Sign Up</button>
+                <LoginModal isSignin={true} />
+                <LoginModal isSignin={false} />
             </div>
         </nav>
     )
