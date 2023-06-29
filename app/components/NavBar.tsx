fce19f00417b0cacb6c5a27664019f269ac0f255
calebtc <calebthecurious@gmail.com>
2023-06-29 17:35:28 +0700

    23rd commit - added AuthModal Content

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,15 +1,15 @@
 "use client";
 
 import Link from "next/link";
-import LoginModal from "./LoginModal";
+import AuthModal from "./AuthModal";
 
 export default function NavBar() {
     return (
         <nav className="bg-white p-2 flex justify-between">
             <Link href="/" className='font-bold text-gray-700 text-2xl'>Locus</Link>
             <div className="flex">
-                <LoginModal isSignin={true} />
-                <LoginModal isSignin={false} />
+                <AuthModal isSignin={true} />
+                <AuthModal isSignin={false} />
             </div>
         </nav>
     )
