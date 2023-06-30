5f732799a71a5e9d06a0db1d4013af5a531a41e0
calebtc <calebthecurious@gmail.com>
2023-06-30 17:14:00 +0700

    24th commit - validating user input

--- app/components/AuthModal.tsx
+++ app/components/AuthModal.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useState } from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
@@ -8,34 +6,49 @@ import Modal from '@mui/material/Modal';
 import AuthModalInputs from './AuthModalInputs';
 
 const style = {
-    position: 'absolute' as 'absolute',
+    position: 'absolute',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
     width: 400,
     bgcolor: 'background.paper',
-
     boxShadow: 24,
     p: 4,
 };
 
-export default function AuthModal({isSignin}:{isSignin: boolean}) {
+export default function AuthModal({ isSignin }: { isSignin: boolean }) {
     const [open, setOpen] = useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
-    const renderContent = (signInContent: string, signupContent: string) => {
-        return isSignin ? signInContent : signupContent;
+    const renderContent = (signInContent: string, signupContent: string) =>
+        isSignin ? signInContent : signupContent;
+
+    const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputs({
+            ...inputs,
+            [e.target.name]: e.target.value
+        })
     }
+
+    const [inputs, setInputs] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        phone: "", 
+        city: "",
+        password: "",
+    });
 
     return (
         <div>
-            <button 
-                className={`${renderContent("bg-blue-400 text-white", "")} "border p-1 px-4 rounded mr-3`}
-                onClick={handleOpen}        
-            >
-                {renderContent("Sign in", "Sign Up")}
-                </button>
+        <button
+            className={`${renderContent("bg-blue-400 text-white", "")} border p-1 px-4 rounded mr-3`}
+            onClick={handleOpen}
+        >
+            {renderContent("Sign in", "Sign Up")}
+            {inputs.email}
+        </button>
         <Modal
             open={open}
             onClose={handleClose}
@@ -43,27 +56,24 @@ export default function AuthModal({isSignin}:{isSignin: boolean}) {
             aria-describedby="modal-modal-description"
         >
             <Box sx={style}>
-                <div className="p-2 h-[400px]">
-                    <div className="uppercase font-bold text-center pb-2 border-b mb-2">
-                        <p className="text-sm">
-                            {renderContent("Sign in", "Create Account")}
-                        </p>
-                    </div>
-                    <div className="m-auto bg-blue-400">
-                        <h2 className="text-2xl font-light text-center">
-                            {renderContent("Log in to your account", "Create your Locus Account")}
-                        </h2>
-                        <AuthModalInputs />
-                        <button
-                            className='uppercase bg-red-600 w-full text-white p-3 rounded text-sm mb-5 disabled:bg-gray-400'
-                        >
-                            {renderContent(
-                                "Sign In", 
-                                "Create Account"    
-                            )}
-                        </button>
-                    </div>
+            <div className="p-2 h-[400px]">
+                <div className="uppercase font-bold text-center pb-2 border-b mb-2">
+                <p className="text-sm">{renderContent("Sign in", "Create Account")}</p>
                 </div>
+                <div className="m-auto bg-blue-400">
+                <h2 className="text-2xl font-light text-center">
+                    {renderContent("Log in to your account", "Create your Locus Account")}
+                </h2>
+                <AuthModalInputs 
+                    inputs={inputs} 
+                    handleChangeInput={handleChangeInput}
+                    isSignin={isSignin}
+                />
+                <button className='uppercase bg-red-600 w-full text-white p-3 rounded text-sm mb-5 disabled:bg-gray-400'>
+                    {renderContent("Sign In", "Create Account")}
+                </button>
+                </div>
+            </div>
             </Box>
         </Modal>
         </div>
