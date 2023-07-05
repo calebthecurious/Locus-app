b65bdce04c2380a53b95a7c9557595df46844450
calebtc <calebthecurious@gmail.com>
2023-07-05 12:47:45 +0700

    27th commit - adding client side validation

--- app/components/AuthModal.tsx
+++ app/components/AuthModal.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
@@ -40,6 +40,29 @@ export default function AuthModal({ isSignin }: { isSignin: boolean }) {
         password: "",
     });
 
+    const [disabled, setDisabled] = useState(true); 
+
+    useEffect(() => {
+        if(isSignin){
+            if(inputs.password && inputs.email){
+                return setDisabled(false)
+            }
+        } else {
+            if (
+                inputs.firstName && 
+                inputs.lastName && 
+                inputs.phone && 
+                inputs.password && 
+                inputs.city && 
+                inputs.email
+            ) {
+                return setDisabled(false);
+            }
+        }
+    
+        setDisabled(true)
+    }, [inputs])
+
     return (
         <div>
         <button
@@ -69,7 +92,10 @@ export default function AuthModal({ isSignin }: { isSignin: boolean }) {
                     handleChangeInput={handleChangeInput}
                     isSignin={isSignin}
                 />
-                <button className='uppercase bg-red-600 w-full text-white p-3 rounded text-sm mb-5 disabled:bg-gray-400'>
+                <button 
+                    className='uppercase bg-red-600 w-full text-white p-3 rounded text-sm mb-5 disabled:bg-gray-400' 
+                    disabled={disabled}
+                >
                     {renderContent("Sign In", "Create Account")}
                 </button>
                 </div>
